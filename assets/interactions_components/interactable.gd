c360fd2d41390106c4001a18c25190969c3c5e7b
M00N2 <Gamersgogaming@protonmail.com>
2025-08-09 02:03:47 -0700

    Added basic interaction system

---
+++ assets/interactions_components/interactable.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export var interact_name: = ""
+@export var is_interactable: bool = true
+
+var interact: Callable = func(): # triggereing function via the interacting componenet
+	pass
